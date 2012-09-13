312f5b48edac9f28af4a1826ca1b87db95c5df9f
Bentotbox <bentotobx@gmail.com>
2012-09-12 21:10:49 -0700

    Now shows up in the wallpaper chooser list.

---
+++ src/com/wallpaper/activity/LauncherActivity.java
@@ -0,0 +1,17 @@
+package com.wallpaper.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class LauncherActivity extends SherlockFragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final Intent intent = new Intent(this, HomeActivity.class);
+		super.startActivity(intent);
+	}
+	
+}
