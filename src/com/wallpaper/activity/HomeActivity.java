0569b235dfda5c452ba67298d6298d5a81bb2215
Ben <bentotbox@gmail.com>
2012-10-12 17:27:44 -0700

    Re designed HomeActivity to simplify and clean up everything.

--- src/com/wallpaper/activity/HomeActivity.java
+++ src/com/wallpaper/activity/HomeActivity.java
@@ -8,7 +8,6 @@ import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.support.v4.app.FragmentTransaction;
@@ -16,7 +15,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.URLUtil;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
@@ -24,9 +25,9 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
-import com.wallpaper.activity.R;
-import com.wallpaper.core.Adapter;
 import com.wallpaper.core.Adapter.OnGetViewListener;
+import com.wallpaper.core.Adapter;
+import com.wallpaper.core.NetworkUtil;
 import com.wallpaper.core.NodeCategory;
 import com.wallpaper.core.NodeWallpaper;
 import com.wallpaper.core.OnFragmentClickListener;
@@ -34,143 +35,165 @@ import com.wallpaper.core.RestClient;
 import com.wallpaper.core.RestClientHandler;
 import com.wallpaper.core.RestClientHandler.OnRestResponseHandler;
 
-public class HomeActivity extends SherlockFragmentActivity implements
-		OnGetViewListener, OnRestResponseHandler, OnNavigationListener,
-		OnFragmentClickListener, OnBackStackChangedListener {
-
+public class HomeActivity extends SherlockFragmentActivity implements 
+	OnRestResponseHandler, OnBackStackChangedListener, OnGetViewListener, OnFragmentClickListener  {
+	
 	private final String TAG = "HomeActivity";
-
-	private final String HIDE = "Hide Launcher Icon";
-	private final String SHOW = "Show Launcher Icon";
-	private final int HIDE_ID = 0;
-	private final int SHOW_ID = 1;
-
-	private final int FLAG_SHOW = PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
-	private final int FLAG_HIDE = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-	private final int DONT_KILL_APP = PackageManager.DONT_KILL_APP;
-
-	private Adapter mAdapter;
+	private final String KEY_LIST_DATA = "list_cache";
+	private final String KEY_LIST_POSITION = "list_position";
+	
 	private ArrayList<NodeCategory> mData;
-	private int mCurrentSelection = -1;
-	private boolean ignoreSelection = false;
-	private boolean ignoreTriggered = false;
-	private final String KEY_DATA = "data_cache";
-	private final String KEY_POSITION = "current_position";
-
-	@SuppressWarnings("unchecked")
+	private int mPosition = -1;
+	private boolean mIgnoreSelection = false;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.getSupportFragmentManager().addOnBackStackChangedListener(this);
-		setContentView(R.layout.activity_home);
-
-		if (savedInstanceState == null
-				|| (savedInstanceState.get(KEY_DATA) == null)) {
-			final String url = super.getResources().getString(
-					R.string.config_wallpaper_manifest_url);
-			RestClient.get(super.getApplicationContext(), url,
-					new RestClientHandler(this));
+		super.setContentView(R.layout.activity_home);
+		
+		this.loadData(savedInstanceState);
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		if(this.mData != null) {
+			outState.putSerializable(KEY_LIST_DATA, this.mData);
+			outState.putInt(KEY_LIST_POSITION, this.mPosition);
 		}
-
-		else {
-			if (savedInstanceState.containsKey(KEY_DATA)) {
-				this.mCurrentSelection = savedInstanceState
-						.getInt(KEY_POSITION);
-				this.onSuccess((ArrayList<NodeCategory>) savedInstanceState
-						.getSerializable(KEY_DATA));
+		super.onSaveInstanceState(outState);
+	}
+	
+	public void loadData(Bundle savedInstanceState) {
+		
+		// Check Network State
+		if(!NetworkUtil.getNetworkState(this)) {
+			final RetryFragment fragment = RetryFragment.getFragmentWithMessage("No connection");
+			this.addFragment(fragment, RetryFragment.TAG, true);
+			return;
+		}
+	
+		if(savedInstanceState == null || savedInstanceState.get(KEY_LIST_DATA) == null) {
+			final String url = super.getResources().getString(R.string.config_wallpaper_manifest_url);
+			if(url != null && URLUtil.isValidUrl(url)) {
+				// Add Loading Fragment
+				final LoadingFragment fragment = new LoadingFragment();
+				this.addFragment(fragment, LoadingFragment.TAG, true);
+				
+				// Load Data
+				final RestClientHandler handler = new RestClientHandler(this);
+				RestClient.get(this, url, handler);
 			}
+		}
+		
+		else {
+			Log.i(TAG, "Restored Instance");
+			this.mData = (ArrayList<NodeCategory>) savedInstanceState.get(KEY_LIST_DATA);
+			this.mPosition = savedInstanceState.getInt(KEY_LIST_POSITION);
+			if(this.mPosition != -1) {
+				mIgnoreSelection = true;
+			}
+
+			this.configureActionBar();
 		}
 	}
 
 	@Override
-	public void onSuccess(ArrayList<NodeCategory> response) {
-		Log.i(TAG, "");
+	public void onResponse(ArrayList<NodeCategory> response) {
 		this.mData = response;
-		this.mAdapter = new Adapter(this, this, this.mData);
-
+		
+		// Add ERROR Fragment
+		if(this.mData == null) {
+			final RetryFragment fragment = RetryFragment.getFragmentWithMessage();
+			this.addFragment(fragment, RetryFragment.TAG, true);
+			return;
+		}
+		
+		if(this.mData.isEmpty()) {
+			this.addFragment(null, null, true);
+			Toast.makeText(getApplicationContext(), "Empty Manifest!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		this.configureActionBar();		
+	}
+	
+	
+	public void configureActionBar() {
 		super.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 		super.getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-		if (!this.mData.isEmpty() && mData.size() == 1) {
+		
+		if(this.mData.size() == 1) {
 			final NodeCategory node = this.mData.get(0);
 			super.getSupportActionBar().setDisplayShowTitleEnabled(true);
 			super.getSupportActionBar().setTitle(node.name);
 			this.onCategorySelected(node);
-			return;
 		}
-
-		super.getSupportActionBar().setDisplayShowTitleEnabled(false);
-		super.getSupportActionBar().setNavigationMode(
-				ActionBar.NAVIGATION_MODE_LIST);
-		super.getSupportActionBar().setListNavigationCallbacks(mAdapter, this);
-		if (this.mCurrentSelection != -1) {
-			this.ignoreSelection = true;
-			super.getSupportActionBar().setSelectedNavigationItem(
-					mCurrentSelection);
+		
+		else {
+			super.getSupportActionBar().setDisplayShowTitleEnabled(false);
+			super.getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+			super.getSupportActionBar().setListNavigationCallbacks(new Adapter(this, this, this.mData), this);
+			if(this.mPosition != -1) {
+				this.mIgnoreSelection = true;
+				super.getSupportActionBar().setSelectedNavigationItem(this.mPosition);
+			}
 		}
 	}
+	
+	
+	
+	public void addFragment(SherlockFragment fragment, String tag, boolean clearStack) {
+		final FragmentManager fm = super.getSupportFragmentManager();
+		final FragmentTransaction transaction = fm.beginTransaction();
+		
+		// Clear Back Stack
+		if(clearStack) {
+			for(int x = 0; x < fm.getBackStackEntryCount(); x++) {
+				fm.popBackStack();
+			}
+		}
+		
+		if(fragment != null) {
+			if(!clearStack) {
+				transaction.replace(R.id.container, fragment, tag);
+				transaction.addToBackStack(null);
+				transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+			}
+			else {
+				transaction.replace(R.id.container, fragment, tag);
+			}
+			
+			try {
+				transaction.commit();
+			} catch (Exception e) { }
+		}
+	}
+
+
+
+	
 
 	@Override
 	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-		this.mCurrentSelection = itemPosition;
-		if (ignoreSelection == true && this.ignoreTriggered == false) {
-			this.ignoreTriggered = true;
+		Log.i(TAG, "Item Selection: " + itemPosition);
+		this.mPosition = itemPosition;
+		if(this.mIgnoreSelection == true) {
+			this.mIgnoreSelection = false;
 			return false;
 		}
-
-		this.onCategorySelected(this.mData.get(mCurrentSelection));
-		return true;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent,
-			LayoutInflater mInflater) {
-		final TextView t = new TextView(this);
-		t.setText(this.mData.get(position).name);
-		t.setTextSize(20);
-		t.setPadding(5, 6, 2, 6);
-		t.setSingleLine(true);
-		return t;
-	}
-
-	public void startFragment(Fragment fragment, String tag, boolean isMain) {
-		final FragmentManager fm = super.getSupportFragmentManager();
-		final FragmentTransaction transaction = fm.beginTransaction();
-
-		if (isMain) {
-			for (int x = 0; x < fm.getBackStackEntryCount(); x++)
-				fm.popBackStack();
-
-			transaction.replace(R.id.container, fragment, tag);
-		} else {
-			transaction.replace(R.id.container, fragment, tag);
-			transaction.addToBackStack(null);
-		}
-
-		transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-		transaction.commit();
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		if (this.mData != null) {
-			outState.putSerializable(KEY_DATA, this.mData);
-			outState.putInt(KEY_POSITION, this.mCurrentSelection);
-		}
-		super.onSaveInstanceState(outState);
+		
+		this.onCategorySelected(this.mData.get(itemPosition));			
+		return false;
 	}
 
 	@Override
 	public void onCategorySelected(NodeCategory node) {
-		if (super.getSupportFragmentManager().findFragmentByTag(WallpaperFragment.FRAGMENT_TAG) != null) {
-			return;
-		}
-
 		final SherlockFragment frag = new CategoryFragment();
 		final Bundle args = new Bundle();
 		args.putSerializable(CategoryFragment.BUNDLE_TAG, node.wallpaperList);
 		frag.setArguments(args);
-		this.startFragment(frag, CategoryFragment.FRAGMENT_TAG, true);
+		this.addFragment(frag, CategoryFragment.FRAGMENT_TAG, true);
 	}
 
 	@Override
@@ -179,20 +202,56 @@ public class HomeActivity extends SherlockFragmentActivity implements
 		final Bundle args = new Bundle();
 		args.putSerializable(WallpaperFragment.BUNDLE_TAG, node);
 		frag.setArguments(args);
-		this.startFragment(frag, WallpaperFragment.FRAGMENT_TAG, false);
+		this.addFragment(frag, WallpaperFragment.FRAGMENT_TAG, false);
 	}
-
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent, LayoutInflater mInflater) {
+		final TextView t = new TextView(this);
+		t.setText(this.mData.get(position).name);
+		t.setTextSize(20);
+		t.setPadding(5, 6, 2, 6);
+		t.setSingleLine(true);
+		return t;
+	}
+	
 	@Override
 	public void onBackStackChanged() {
 		final FragmentManager fm = super.getSupportFragmentManager();
 		if (fm.getBackStackEntryCount() == 0) {
-			this.ignoreSelection = false;
-			this.ignoreTriggered = false;
-			this.onSuccess(this.mData);
+			this.configureActionBar();
 		}
 	}
 
-	@SuppressWarnings("deprecation")
+
+
+	
+	
+	
+	
+	
+	
+	private final String HIDE = "Hide Launcher Icon";
+	private final String SHOW = "Show Launcher Icon";
+	private final int HIDE_ID = 0;
+	private final int SHOW_ID = 1;
+	
+	private final int FLAG_SHOW = PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+	private final int FLAG_HIDE = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+	private final int DONT_KILL_APP = PackageManager.DONT_KILL_APP;
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.clear();
+		menu.add(3, 3, 3, ((isShowing()) ? HIDE : SHOW)).setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -216,19 +275,6 @@ public class HomeActivity extends SherlockFragmentActivity implements
 				}
 			}
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		menu.clear();
-		menu.add(3, 3, 3, ((isShowing()) ? HIDE : SHOW)).setShowAsAction(
-				MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
-		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
@@ -305,5 +351,8 @@ public class HomeActivity extends SherlockFragmentActivity implements
 		ComponentName c = new ComponentName(this, LauncherActivity.class);
 		return c;
 	}
-
+	
+	
+	
+	
 }
