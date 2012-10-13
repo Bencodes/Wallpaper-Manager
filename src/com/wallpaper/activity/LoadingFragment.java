438d05ca14da28c44d154ed69c963b5ceeaa0b63
Ben <bentotbox@gmail.com>
2012-10-12 17:25:57 -0700

    Added a loading/pending fragment to show a progress spinner

---
+++ src/com/wallpaper/activity/LoadingFragment.java
@@ -0,0 +1,19 @@
+package com.wallpaper.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class LoadingFragment extends SherlockFragment {
+	
+	public static final String TAG = "RetryFragment";
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_loading, null, false);
+	}
+}
