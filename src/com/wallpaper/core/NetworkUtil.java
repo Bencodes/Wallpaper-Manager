692e16eacc3403f859a379b181dfd6cca8f5eaf5
Ben <bentotbox@gmail.com>
2012-10-12 17:25:11 -0700

    Check Network State

---
+++ src/com/wallpaper/core/NetworkUtil.java
@@ -0,0 +1,22 @@
+package com.wallpaper.core;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+	public static boolean getNetworkState(Context c) {
+		ConnectivityManager connectivity = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity != null) {
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null) {
+				for (int i = 0; i < info.length; i++) {
+					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
